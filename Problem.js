4c6c0a211c978aad39182e762f6cc20712a316bb
miftarek <miftadc@gmail.com>
2023-08-08 23:49:54 +0600

    js practice ongoing

---
+++ Problem.js
@@ -0,0 +1,32 @@
+/* function cubeNumber(number){
+    let result = number * number * number;
+    console.log(result);
+    return result;
+}
+
+cubeNumber("tarek"); */
+
+// -----------------------------
+
+function matchFinder(string1, string2) {
+    // const string1 = 'John Doe'
+    const existString = string1.includes(string2);
+    console.log(existString);
+    }
+
+matchFinder('Peter Parker','Pet');
+
+// -----------------------------
+
+/* function sortMaker(arr) {
+    for (let i = arr.length-1; i >= 0; i--){
+        let element = arr[i];
+        console.log(element);
+        }
+    }
+
+const arr = [4, -2];
+sortMaker(arr); */
+
+// -----------------------------
+
